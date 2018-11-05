feaf3b361c963e2f00d900ed28016e303878d810
slak1 <44374924+slak1@users.noreply.github.com>
2018-11-05 10:34:21 +0100

    Update DoReq.py

--- DoReq.py
+++ DoReq.py
@@ -1,7 +1,7 @@
 import requests ,sys  ,os ,importlib
 arg=sys.argv;
 if len(arg)<4:
-    print('DoReq.py PythonFromCurlFile login pass')
+    print('DoReq4.py NamePythonModule')
     sys.exit();
 arg1=arg[1];
 if arg1[len(arg1)-3:]=='.py': arg1=arg1[:-3];
@@ -23,6 +23,24 @@ dane=reqMod;
 dane.url= reqMod.headers["Origin"];
 dane.login=arg[2];
 dane.passw=arg[3];
+#dane.data=reqMod.data;
+#dane.headers=reqMod.headers;
+
+print(dane.headers)
+
+def Readln(filepath):
+   global dane
+   filepath=filepath+'.py'
+   with open(filepath) as fp:
+    line = fp.readline()
+    while line:
+     l=line.strip(); dl=len(l);
+     if l[:8]=='response':
+        a=l.find('\'')+1
+        b=l.rfind('\'')
+        return l[a:b]
+     line = fp.readline()
+
 
 def main():
  global dane
@@ -32,6 +50,7 @@ def main():
   for key, value in dane.data.items():
      print(key, '----------', value )
   print("____________________________________")
+
 
  def transf():
   global dane
@@ -43,20 +62,27 @@ def main():
        if postfix=='^':
         #print(dane.data[key])
         dane.data[key]=value[:x-1]
-        #print(key,"-------",dane.data[key])
-  #print("-------END TRASFORM---------")
+        print(key,"-------",dane.data[key])
+  print("-------END TRASFORM---------")
+
 
  print(dane.data)
  transf();
  seeData()
+ #print (Readln(arg1))
 
 if __name__ == '__main__':
     main()
+    print (Readln(arg1))
+    #sys.exit();
     with requests.Session() as s:
+     #s.get(dane.url, auth=('admin', 'zlotko14n'))
      s.get(dane.url, auth=(dane.login, dane.passw))
-     r = dane.response;
+     #r = s.post(dane.url+'/apply.cgi', headers=dane.headers, data=dane.data)
+     #r = dane.response;
+     r = s.post(Readln(arg1), headers=dane.headers, data=dane.data)
 
      #print(r.text)
-     print('Output html from: ',dane.url,"DoReq-Output.html")
-     with open("DoReq-Output.html", "wb") as f:
+     print('Zapis')
+     with open("DoReq5.html", "wb") as f:
       f.write(r.content)
